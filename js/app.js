c68943683ffb3be3da5909c31702f17fb15628c5
w-ziem <143935791+w-ziem@users.noreply.github.com>
2025-02-25 22:01:48 +0100

    Add filtering monthly summary by current month

--- js/app.js
+++ js/app.js
@@ -2,23 +2,31 @@
 import { TRANSACTIONS } from "./modal.js";
 
 
+const CURRDATE = new Date();
+const CURRMONTH = CURRDATE.getMonth() + 1;
+console.log("read current month", CURRMONTH)
+
+
 export function calculateTotalIncomeAndExpense(){
     //get current amount or start with 0
     let income = Number(document.querySelector("#total-income").textContent.substring(1)) || 0; 
     let expense = Number(document.querySelector("#total-expense").textContent.substring(1)) || 0;
     console.log([income, expense]);
-    console.log(document.querySelector("#total-income").textContent)
     TRANSACTIONS.forEach(entry => {
-        console.log(entry)
-        if(entry.category === "income" && ! entry.caculated){
-            income += Number(entry.amount);
-            entry.caculated = true; //ensure same transaction is only calculated once
-        } else if( ! entry.caculated ){
-            expense += Number(entry.amount);
-            entry.calculated = true; //ensure same transaction is only calculated once
+        console.log("current read entry when calculating total income and expense: ", entry);
+        const entryMonth = Number(entry.date.split("-")[1]);
+        console.log("currently reading transaction month: ", entryMonth);
+        if(entryMonth === CURRMONTH){
+            if(entry.category === "income" && ! entry.caculated){
+                income += Number(entry.amount);
+                entry.caculated = true; //ensure same transaction is only calculated once
+            } else if( ! entry.caculated ){
+                expense += Number(entry.amount);
+                entry.calculated = true; //ensure same transaction is only calculated once
+            }
         }
     });
-    console.log([income, expense]);
+    console.log("calculateTotalIncomeAndExpense returns: ", [income, expense]);
     return [income.toFixed(2), expense.toFixed(2)];
 }
 
@@ -39,5 +47,5 @@ let categoryTotal = {
 
 
 // function calculatePerCategory(){
-//     ...
+//      ...
 // }
